1a595a1b9c76a2c76808cdd43a4816c5e77c2ebf
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-09-30 15:58:51 -0500

    feat: create function to create AFN Graph

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,4 +1,17 @@
-export function validate(regex: string) {
+import { createBase, createKleenLock, createOR } from "../objects/RCreator";
+import RGraph from "../objects/RGraph";
+
+export function validate(regex: string): boolean {
     // Validate regex
     return true;
+}
+
+// Get regex's alphabet
+export function getAPH(regex: string): string {
+    return '';
+}
+
+// Create AFN Graph
+export function createGraph(regex: string): RGraph | undefined {
+    return;
 }
