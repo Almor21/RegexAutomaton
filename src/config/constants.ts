30036f70d482327ce875f1a31c5936a8406eaf6a
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-10-02 01:19:53 -0500

    feat: create get functions for Alphabet and Transition Table

---
+++ src/config/constants.ts
@@ -0,0 +1,7 @@
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const VALID_LETTERS = letters.split('');
+
+const symbols = "*?|+";
+export const VALID_SYMBOLS = symbols.split('');
+
+export const VALID_CHARS = symbols.concat(letters);
