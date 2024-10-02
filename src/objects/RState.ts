680bb3e0ed9e962bfaa0edf2f55cabbb046977d0
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-10-02 01:19:53 -0500

    feat: create get functions for Alphabet and Transition Table

--- src/objects/RState.ts
+++ src/objects/RState.ts
@@ -4,16 +4,26 @@ import RConnection from './RConnection';
 // AFN State
 class RState {
     ID: string;
+    private label: string;
     connections: RConnection[];
 
-    constructor() {
+    constructor(label: string = '') {
         this.ID = getUniqueID();
+        this.label = label;
         this.connections = [];
     }
 
     addConnection = (connection: RConnection) => {
         this.connections.push(connection);
     };
+
+    setLabel = (label: string) => {
+        this.label = label;
+    }
+
+    getLabel = () => {
+        return this.label || this.ID;
+    }
 }
 
 export default RState;
--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,5 +1,6 @@
-import { createBase, createKleenLock, createOR } from "../objects/RCreator";
-import RGraph from "../objects/RGraph";
+import { VALID_LETTERS } from '../config/constants';
+import { createBase, createKleenLock, createOR } from '../objects/RCreator';
+import RGraph from '../objects/RGraph';
 
 export function validate(regex: string): boolean {
     // Validate regex
@@ -7,11 +8,34 @@ export function validate(regex: string): boolean {
 }
 
 // Get regex's alphabet
-export function getAPH(regex: string): string {
-    return '';
+export function getAPH(regex: string): string[] {
+    return Array.from(
+        new Set(
+            regex.split('').filter((c) => VALID_LETTERS.some((lt) => lt === c))
+        )
+    ).sort();
 }
 
 // Create AFN Graph
 export function createGraph(regex: string): RGraph | undefined {
-    return;
+    const a = createBase('a');
+
+    if (a) return createKleenLock(a);
+}
+
+// Get AFN Transition Table
+export function getTransitionTable(graph: RGraph, columns: string[]) {
+    return Object.fromEntries(
+        graph.states.map((st) => [
+            st.getLabel(),
+            Object.fromEntries(
+                columns.map((cl) => [
+                    cl,
+                    st.connections
+                        .find((cn) => cn.value === cl)
+                        ?.next.getLabel()
+                ])
+            )
+        ])
+    );
 }
