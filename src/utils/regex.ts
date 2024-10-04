59735d39d37f2351fedb65768d3877f92e257541
Fernando <fmvalencia@uninorte.edu.co>
2024-10-03 21:22:34 -0500

    task: implement validation for input

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,8 +2,21 @@ import { createBase, createKleenLock, createOR } from "../objects/RCreator";
 import RGraph from "../objects/RGraph";
 
 export function validate(regex: string): boolean {
-    // Validate regex
-    return true;
+    try {
+        // block the use of the following characters: \ [ ] . ^ $
+        const forbiddenPattern = /[\\\[\]\.\^\$]/;
+        
+        // verify if the regex contains forbidden characters
+        if (forbiddenPattern.test(regex)) {
+            return false;
+        }
+
+        // we tried to compile the regex
+        new RegExp(regex);
+        return true;  // if it compiles, it is valid
+    } catch (e) {
+        return false;  // if it doesn't compile, it is invalid
+    }
 }
 
 // Get regex's alphabet
