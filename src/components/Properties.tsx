1fd39c586fc1167d88751bf3c199f69b27fd8e1e
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-10-07 19:33:43 -0500

    style: improve design

--- src/components/Properties.tsx
+++ src/components/Properties.tsx
@@ -76,6 +76,7 @@ function Properties({
             setFinalStates(
                 graph.finalState ? [graph.finalState.getLabel()] : []
             );
+            setTotalStates(graph.states.length);
         }
     }, [graph]);
 
@@ -95,12 +96,11 @@ function Properties({
             </motion.div>
             <motion.div
                 ref={openDiv}
-                className="absolute top-1/2 left-2 z-10 w-64 h-[95%] grid grid-rows-[auto_1fr] bg-[var(--color-300)] rounded-xl p-4 shadow-md"
+                className="absolute top-1/2 left-2 z-10 w-64 h-[95%] grid grid-rows-[auto_1fr] bg-[var(--color-300)] rounded-xl p-4 shadow-[1px_3px_6px_rgba(0,0,0,0.4)]"
                 style={{
                     x: '-110%',
                     y: '-50%'
                 }}
-                onClick={() => setOpen(false)}
             >
                 <div className="grid grid-cols-[1fr_auto] items-center">
                     <h1 className="text-white py-1 row-start-1 col-start-1 col-end-3 text-center border-b-2 border-white">
@@ -114,9 +114,14 @@ function Properties({
                         <span className="absolute top-1/2 left-0 -translate-y-1/2 inline-block bg-white w-full h-1 rounded-full rotate-45" />
                     </div>
                 </div>
-                <div className="flex flex-col gap-2 h-full overflow-x-auto">
+                <div className="h-full px-2 py-2 flex flex-col gap-2 overflow-x-auto">
+                    {/* container of states (property) */}
+                    <div className="bg-[var(--color-500)] p-3 rounded-lg text-white ">
+                        <h3 className="font-semibold mb-2 text-center">Test String</h3>
+                        <p className="overflow-x-auto h-6">{str}</p>
+                    </div>
                     {/* Alphabet container */}
-                    <div className="bg-[var(--color-500)] p-3 rounded-lg mt-2 text-white shadow-md max-h-24 overflow-y-auto">
+                    <div className="h-32 bg-[var(--color-500)] p-3 rounded-lg text-white shadow-md">
                         <h3 className="font-semibold mb-2 text-center">
                             Alphabet
                         </h3>
@@ -125,30 +130,21 @@ function Properties({
                         </p>
                     </div>
                     {/* container of some info */}
-                    <div className="bg-[var(--color-500)] p-4 rounded-lg text-white">
+                    <div className="bg-[var(--color-500)] p-3 rounded-lg text-white">
                         <h3 className="font-semibold mb-2 text-center">
                             {option === 2 ? 'AFN' : 'AFD'}
                         </h3>
                         <p>Initial state: {initialState}</p>
                         <p>Final state: {finalStates.join(', ')}</p>
-                        <p>Total of states: {totalStates}</p>
+                        <p>Total of states: {totalStates ? totalStates : ''}</p>
                     </div>
                     {/* container of states (property) */}
-                    <div className="bg-[var(--color-500)] p-4 rounded-lg text-white">
+                    <div className="bg-[var(--color-500)] p-3 rounded-lg text-white">
                         <h3 className="font-semibold mb-2 text-center">
                             States
                         </h3>
                         // show the selected state
                     </div>
-                    {/* container of states (property) */}
-                    {str && (
-                        <div className="bg-[var(--color-500)] p-4 rounded-lg mb-2 text-white ">
-                            <h3 className="font-semibold mb-2 text-center">
-                                STR
-                            </h3>
-                            <p className="overflow-x-auto">{str}</p>
-                        </div>
-                    )}
                 </div>
             </motion.div>
         </>
