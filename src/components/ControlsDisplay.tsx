979d33f51a51fae3e23ac33d93d4416d82103bf2
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-10-07 19:07:58 -0500

    feat: add image controls

--- src/components/ControlsDisplay.tsx
+++ src/components/ControlsDisplay.tsx
@@ -12,25 +12,43 @@ function ControlsDisplay({ controls }: { controls?: ControlsType }) {
     const onStop = controls?.stop;
 
     return (
-        <div className="absolute p-1 bottom-3 left-1/2 z-10 flex gap-3 -translate-x-1/2 bg-[var(--color-300)] border border-[var(--color-700)] rounded-sm">
+        <div className="absolute p-2 bottom-3 left-1/2 z-10 flex gap-3 -translate-x-1/2 bg-[var(--color-300)] border border-[var(--color-700)] rounded-md">
             <button
-                className="w-6 h-6 bg-green-500 transition-all hover:bg-green-600 active:bg-green-700 rounded-md"
+                className="w-6 h-6 p-1 bg-green-500 transition-all hover:bg-green-600 active:bg-green-700 rounded-md"
                 onClick={() => {
                     if (onPlay) onPlay();
                 }}
-            ></button>
+            >
+                <img
+                    src="/play.svg"
+                    alt="play icon"
+                    className="h-full w-full"
+                />
+            </button>
             <button
-                className="w-6 h-6 bg-yellow-500 transition-all hover:bg-yellow-600 active:bg-yellow-700 rounded-md"
+                className="w-6 h-6 p-1 bg-yellow-500 transition-all hover:bg-yellow-600 active:bg-yellow-700 rounded-md"
                 onClick={() => {
                     if (onStep) onStep();
                 }}
-            ></button>
+            >
+                <img
+                    src="/step.svg"
+                    alt="step icon"
+                    className="h-full w-full"
+                />
+            </button>
             <button
-                className="w-6 h-6 bg-red-500 transition-all hover:bg-red-600 active:bg-red-700 rounded-md"
+                className="w-6 h-6 p-1 bg-red-500 transition-all hover:bg-red-600 active:bg-red-700 rounded-md"
                 onClick={() => {
                     if (onStop) onStop();
                 }}
-            ></button>
+            >
+                <img
+                    src="/stop.svg"
+                    alt="stop icon"
+                    className="h-full w-full"
+                />
+            </button>
         </div>
     );
 }
